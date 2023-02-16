dace5b3fdb9c928cc20fecfe5b371ffe1e7fec5e
sowmik-sec <sowmik.sec@gmail.com>
2023-02-16 15:12:15 +0600

    making responsive navbar with tailwind css and react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1 className="text-5xl underline">inside h1</h1>
       <p>paragraph</p>
     </div>
